8fc5ddc7177bc0164ae335dd25f94c9ca301c0bf
kybu <kybu@kybu.org>
2017-01-20 19:28:25 +0100

    Resert mysql root password. Create kyberia user and database.

--- docker/run_percona.sh
+++ docker/run_percona.sh
@@ -3,4 +3,4 @@
 set -x
 
 /usr/bin/mysqld_pre_systemd
-/usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid
+/usr/sbin/mysqld  --validate-password=OFF --pid-file=/var/run/mysqld/mysqld.pid $@ || echo "Failed"
